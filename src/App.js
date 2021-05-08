37d6c7b2425748b3018d72041df4ec3ea504667d
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2021-05-08 12:52:38 +0300

    Remove data atrebute from image item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./views/HomePage";
+import MoviesPage from "./views/MoviesPage";
+import MovieDetailsPage from "./views/MovieDetailsPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact="/movies/:movieId" component={MoviesPage} />
+        <Route path="/movie" component={MoviesPage} />
+      </Switch>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/views/MoviesPage/MoviesPage.js
@@ -0,0 +1,5 @@
+const MoviesPage = () => {
+  return <h1>MoviesPage</h1>;
+};
+
+export default MoviesPage;
